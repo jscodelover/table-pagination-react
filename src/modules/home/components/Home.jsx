fb80068f8769e0b2771ed329a7f9e2cfdc8dbc4b
jscodelover <manisha.4918@gmail.com>
2019-04-09 21:56:13 +0530

    refactor: store data in component state

--- src/modules/home/components/Home.jsx
+++ src/modules/home/components/Home.jsx
@@ -1,43 +1,66 @@
 import React from "react";
+import axios from "axios";
 import StyleHome from "./home.style";
+import { Loading } from "../../../components";
 
-const Home = () => {
-  function heading(columnName) {
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    axios
+      .get("http://demo9197058.mockable.io/users")
+      .then(({ data }) => this.setState({ data, loading: false }))
+      .catch(err => this.setState({ loading: false }));
+  }
+
+  heading = columnName => {
     return columnName.map(col => (
       <th key={col} className="table__row--content table__row--column-heading">
         {col}
       </th>
     ));
+  };
+
+  render() {
+    const { loading } = this.state;
+    if (loading) return <Loading />;
+    return (
+      <StyleHome>
+        <table className="table">
+          <caption className="table__caption">
+            <span className="table__caption--text1">Data Peace</span>
+            <br />
+            <span className="table__caption--text2">Front End Task</span>
+          </caption>
+          <thead>
+            <tr className="table__row">
+              {this.heading([
+                "first name",
+                "last name",
+                "company name",
+                "city",
+                "state",
+                "zip",
+                "email",
+                "web",
+                "age"
+              ])}
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="table__row" />
+          </tbody>
+        </table>
+      </StyleHome>
+    );
   }
-  return (
-    <StyleHome>
-      <table className="table">
-        <caption className="table__caption">
-          <span className="table__caption--text1">Data Peace</span>
-          <br />
-          <span className="table__caption--text2">Front End Task</span>
-        </caption>
-        <thead>
-          <tr className="table__row">
-            {heading([
-              "first name",
-              "last name",
-              "company name",
-              "city",
-              "state",
-              "zip",
-              "email",
-              "web",
-              "age"
-            ])}
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="table__row" />
-        </tbody>
-      </table>
-    </StyleHome>
-  );
-};
+}
 
 export default Home;
